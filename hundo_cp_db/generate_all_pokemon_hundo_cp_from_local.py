8848eefa09f17e3d62500274f524016bd7b6ad96
Phu Nguyen <phulnguyen7@gmail.com>
2025-10-28 17:13:28 -0500

    1. bug fix for access dict item 2. clean up

--- hundo_cp_db/generate_all_pokemon_hundo_cp_from_local.py
+++ hundo_cp_db/generate_all_pokemon_hundo_cp_from_local.py
@@ -1,7 +1,6 @@
 from get_main_to_base import extract_main_series_stats, calculate_pogo_base_stats
 from calculate_cp import calculate_hundo_cp_dict, load_cpm_table
 import json
-import time
 
 if __name__ == "__main__":
     index = 0
@@ -22,30 +21,30 @@ if __name__ == "__main__":
         
         for pokemon in pokemon_list:
             index += 1
-            print(f"Processing Pokémon #{index}: {pokemon.forms[0]['name']}")
+            print(f"Processing Pokémon #{index}: {pokemon['forms'][0]['name']}")
             
             pokemon_main_stat = extract_main_series_stats(pokemon)
-            pokemon_main_stat["name"] = pokemon.forms[0]['name']
-            output_main_stats_filename.write(json.dumps(pokemon_main_stat) + '\n')
-            output_main_stats_filename.flush()
+            pokemon_main_stat["name"] = pokemon['forms'][0]['name']
+            output_main_stats_file.write(json.dumps(pokemon_main_stat) + '\n')
+            output_main_stats_file.flush()
             
             pokemon_go_base_stat = calculate_pogo_base_stats(pokemon_main_stat)
-            pokemon_go_base_stat["name"] = pokemon.forms[0]['name']
-            output_go_base_stats_filename.write(json.dumps(pokemon_go_base_stat) + '\n')
-            output_go_base_stats_filename.flush()
+            pokemon_go_base_stat["name"] = pokemon['forms'][0]['name']
+            output_go_base_stats_file.write(json.dumps(pokemon_go_base_stat) + '\n')
+            output_go_base_stats_file.flush()
             
             hundo_cp_dict = {}
             hundo_cp_dict["ndex"] = index
-            hundo_cp_dict["name"] = pokemon.forms[0]['name']
+            hundo_cp_dict["name"] = pokemon['forms'][0]['name']
             hundo_cp_dict = hundo_cp_dict | calculate_hundo_cp_dict(pokemon_go_base_stat, 15, CPM_TABLE)
-            output_hundo_cp_filename.write(json.dumps(hundo_cp_dict) + '\n')
-            output_hundo_cp_filename.flush()
-            print(f"Hundo CP data for {pokemon.forms[0]['name']} successfully written.")
+            output_hundo_cp_file.write(json.dumps(hundo_cp_dict) + '\n')
+            output_hundo_cp_file.flush()
+            print(f"Hundo CP data for {pokemon['forms'][0]['name']} successfully written.")
 
         input_pokemon_file.close()
-        output_main_stats_filename.close()
-        output_go_base_stats_filename.close()	
-        output_hundo_cp_filename.close()
+        output_main_stats_file.close()
+        output_go_base_stats_file.close()	
+        output_hundo_cp_file.close()
     except FileNotFoundError:
         print(f"Error: The file {input_pokemon_filename} was not found.")
         exit(1)
@@ -55,23 +54,3 @@ if __name__ == "__main__":
     except IOError as e:
         print(f"Error accessing or writing to file {output_hundo_cp_filename}: {e}")
         exit(3)
-
-"""
-                hundo_cp_dict = {}
-                hundo_cp_dict["Ndex"] = index
-                hundo_cp_dict["Name"] = pokemon['name']
-                hundo_cp_dict = hundo_cp_dict | calculate_hundo_cp_dict(pokemon_go_base_stat, 15, CPM_TABLE)
-                
-                # Write the dictionary immediately and append a newline for JSON Lines format
-                json.dump(hundo_cp_dict, outfile)
-                outfile.write('\n')
-                # Flush the write buffer to the disk immediately (optional, but safer)
-                outfile.flush()
-                
-                print(f"Hundo CP data for {pokemon['name']} successfully written.")
-
-                time.sleep(1)  # To avoid hitting API rate limits
-                
-        print(f"All Hundo CP data successfully processed and written to {output_hundo_cp_filename}.")
-"""    
-
